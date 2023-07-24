18aa873d7d8cca44b8dcf66fe79de78be0002324
neverett8fr <neverett8fr@btinternet.com>
2023-07-24 23:40:34 +0100

    add endpoint names

--- pkg/application/service/service.go
+++ pkg/application/service/service.go
@@ -8,13 +8,35 @@ import (
 )
 
 func NewHome(r *mux.Router) {
-	r.HandleFunc("/test/{text}", testHandler).Methods("GET")
+	r.HandleFunc("/test/{text}", testHandler).Methods(http.MethodGet)
+	r.HandleFunc("/endpoint/{name}", addEndpointHandler).Methods(http.MethodPost)
+	r.HandleFunc("/condition/{name}", addConditionHandler).Methods(http.MethodPost)
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
 	text := params["text"]
+
+	body := entities.NewResponse(text)
+
+	writeReponse(w, r, body)
+}
+
+func addEndpointHandler(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	text := params["name"]
+
+	body := entities.NewResponse(text)
+
+	writeReponse(w, r, body)
+}
+
+func addConditionHandler(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	text := params["name"]
 
 	body := entities.NewResponse(text)
 
